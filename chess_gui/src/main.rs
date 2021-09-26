e053379f817ecf20c1c4e6618324f82cda1a2524
Vincent <vincent.lagerros@gmail.com>
2021-09-26 02:29:58 +0200

    super basic test ggez

--- chess_gui/src/main.rs
+++ chess_gui/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+//! The simplest possible example that does something.
+#![allow(clippy::unnecessary_wraps)]
+
+use ggez::event;
+use ggez::graphics::{self, Color};
+use ggez::{Context, GameResult};
+use glam::*;
+
+struct MainState {
+    pos_x: f32,
+}
+
+impl MainState {
+    fn new() -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0 };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            Vec2::new(0.0, 0.0),
+            100.0,
+            2.0,
+            Color::WHITE,
+        )?;
+        graphics::draw(ctx, &circle, (Vec2::new(self.pos_x, 380.0),))?;
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let (ctx, event_loop) = cb.build()?;
+    let state = MainState::new()?;
+    event::run(ctx, event_loop, state)
 }
